fe32b2dfec1d08b99d280d2cee9102467da0dd07
NoPainToday5 <hed4123@gmail.com>
2024-11-11 16:59:17 +0100

    Ceasar cypher finished

--- Ceasar's_cypher.py
+++ Ceasar's_cypher.py
@@ -3,22 +3,46 @@ def cipher(file, num):
         text = file.read()
 
     ciphertext = ""
-    
+
     for i in text:
         if(i == " " or i == "\n"):
-            continue
+            ciphertext += i
         else:
-            if(ord(i) >= 65 and ord(i) <= 90):
-                if((ord(i)+num) > 90):
-                    ciphertext += chr( ((ord(i) + num) % 90) + 65 )
-                else:
-                    ciphertext += chr( ((ord(i) + num) % 90))
+            if(i.isupper()):
+                ciphertext += chr((ord(i) + num - 65) % 26 + 65)
+
+            if(i.islower()):
+                ciphertext += chr((ord(i) + num - 97) % 26 + 97)
+    
+    return ciphertext
 
 
-    return
+def decypher(text, num, file, opt):
+
+    decrypted_txt = ""
+
+    for i in text:
+        if(i == " " or i == "\n"):
+            decrypted_txt += i
+        else:
+            if(i.isupper()):
+                decrypted_txt += chr((ord(i) - num - 65) % 26 + 65)
+
+            if(i.islower()):
+                decrypted_txt += chr((ord(i) - num - 97) % 26 + 97)
+    
+    with open(file, opt) as file:
+        if(opt == "w"):
+            file.write(decrypted_txt)
+        if(opt == "a"):
+            file.write(decrypted_txt)
+    
+    return decrypted_txt
 
 file_name = "./Files/cypher.txt"
 
-print(chr( ((ord("Z") + 0) - 90) + 65))
+encrypted = cipher(file_name, 23)
+print(encrypted)
 
-
+decrypted = decypher(encrypted, 23, file_name, "w")
+print(decrypted)
